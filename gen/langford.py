e78a27a31ec067b3d00552810a94c7f5aa66be77
Aaron Windsor <aaron.windsor@gmail.com>
2021-07-24 07:50:41 -0400

    document langford generator

--- gen/langford.py
+++ gen/langford.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+
+# From https://en.wikipedia.org/wiki/Langford_pairing:
+# A Langford pairing is a permutation of the sequence of 2n numbers
+# 1, 1, 2, 2, ..., n, n in which the two 1s are one unit apart, the
+# two 2s are two units apart, and more generally the two copies of
+# each number k are k units apart.
+
+# Langford pairings are only possible when n = 0 mod 4 or n = 3 mod 4.
+
 import sys
 
 def langford(n):
@@ -18,4 +27,10 @@ if __name__ == '__main__':
     except:
         print('Usage: "langford.py n"')
         sys.exit(-1)
+    print("# Langford pairings for n = {}.".format(n))
+    print("# Items 1 ... n represent the choice of a number.")
+    print("# Items s1 ... s2n represent the positions of a number.")
+    print("# Options of the form \"1 s1 s3\" represent valid")
+    print("# placements: \"1 is placed in position 1 and 3\"")
+    print()
     langford(n)
--- test/langford_3.xc
+++ test/langford_3.xc
@@ -1,3 +1,9 @@
+# Langford pairings for n = 3.
+# Items 1 ... n represent the choice of a number.
+# Items s1 ... s2n represent the positions of a number.
+# Options of the form "1 s1 s3" represent valid
+# placements: "1 is placed in position 1 and 3"
+
 1 2 3 s1 s2 s3 s4 s5 s6
 1 s1 s3
 1 s2 s4
