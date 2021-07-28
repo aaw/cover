953c7d9887d6a6859ddf5dbf21460e7de91daad2
Aaron Windsor <aaron.windsor@gmail.com>
2021-07-28 19:55:17 -0400

    add some scripts around sudoku solving

---
+++ translate/solve-all-hard-sudokus.sh
@@ -0,0 +1,9 @@
+# Solves all 95 hard sudoku puzzles from a list on magictour.free.fr.
+
+SCRIPT="$(realpath $0)"
+SCRIPTDIR="$(dirname $SCRIPT)"
+for puzzle in $(wget -qO- http://magictour.free.fr/top95); do
+    [ "$(expr length $puzzle)" -ne 81 ] && continue
+    echo "Solving [${puzzle}]"
+    "${SCRIPTDIR}/solve-sudoku.sh" "$puzzle"
+done
