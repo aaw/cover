dfc13be9f321796a8aa22c27e2af7731dfc22f8c
Aaron Windsor <aaron.windsor@gmail.com>
2021-07-31 05:53:58 -0400

    move sudoku utils to subdirectory

--- examples/solve-sudoku.sh
+++
@@ -1,6 +0,0 @@
-SCRIPT="$(realpath $0)"
-SCRIPTDIR="$(dirname $SCRIPT)"
-BASEDIR="$(dirname $SCRIPTDIR)"
-cd "$BASEDIR"
-make 1>/dev/null
-./bin/xc <(./translate/encode-sudoku.py "$1") | ./translate/decode-sudoku.py "$1"
---
+++ examples/sudoku/solve-sudoku.sh
@@ -0,0 +1,7 @@
+SCRIPT="$(realpath $0)"
+SCRIPTDIR="$(dirname $SCRIPT)"
+EXAMPLEDIR="$(dirname $SCRIPTDIR)"
+BASEDIR="$(dirname $EXAMPLEDIR)"
+cd "$BASEDIR"
+make 1>/dev/null
+./bin/xc <(./examples/sudoku/encode-sudoku.py "$1") | ./examples/sudoku/decode-sudoku.py "$1"
