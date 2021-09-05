9c62f0837b3f4e7552a56c60fb442af11f2fafb6
Aaron Windsor <aaron.windsor@gmail.com>
2021-09-05 13:38:00 -0400

    helper script for wordcross examples

---
+++ examples/wordcross/create-wordcross.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Generate wordcross placements. Usage: create-wordcross.sh wordfile numrows numcols
+# generates wordcross tilings that are numrows x numcols and contain all of
+# the words in wordfile. Words in wordfile should be one per line.
+
+SCRIPT="$(realpath $0)"
+SCRIPTDIR="$(dirname $SCRIPT)"
+EXAMPLEDIR="$(dirname $SCRIPTDIR)"
+BASEDIR="$(dirname $EXAMPLEDIR)"
+cd "$BASEDIR"
+make 1>/dev/null
+./bin/xcc <(./examples/wordsearch/encode-wordcross.py "$1" "$2" "$3") | ./examples/wordsearch/decode-wordcross.py "$2" "$3"
